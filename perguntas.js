015b544c98bf9196e7e1a9c88d623c32108a8040
agnesbrenda01 <agnesb344@gmail.com>
2024-11-19 12:19:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'futebol masculino',
+    'em qual ano o corithians ganhou o mundial.?',
+    'no ano de 2012'
+)
+
+criaCartao (
+    'FUTEBOL MASCULINO',
+    'Qual o melhor jogador do brasileirao de 2024?',
+    'Yuri Alberto'
+)
+
+criaCartao (
+    'Volei feminino',
+    'quem e a capita da selacao brasileira de volei?',
+    'Gabi'
+)
+
+criaCartao (
+    'volei feminino?',
+    'qual a ruiva mais linda da selecao brasileira?',
+    'Julia Bergmanns'
+)
